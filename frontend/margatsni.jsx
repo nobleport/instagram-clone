9b7ffc9c50afa966d8096a7f204a3e8e50102955
John Gardner <john.s.gardner@gmail.com>
2022-05-18 13:37:06 -0700

    Seed database

--- frontend/margatsni.jsx
+++ frontend/margatsni.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
-import Root from "./components/root"
+import Root from "./root"
 import {login} from "./actions/session_actions"
 
 document.addEventListener("DOMContentLoaded", ()=>{
